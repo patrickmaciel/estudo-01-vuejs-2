6ffd245710ead175b7462510989cfed56983706e
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-04-19 18:58:50 -0300

    Componentes com componentes

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+Vue.component('task-list', {
+    template: `
+    <div>
+        <task v-for="task in tasks">{{ task.name }}</task>
+    </div>
+    `,
+    data() {
+        return {
+            tasks: [
+                { name: 'Jesus', completed: false },
+                { name: 'te', completed: true },
+                { name: 'ama!', completed: false },
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
    template: '<li><slot></slot></li>'
 });
