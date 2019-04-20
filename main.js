c6e343dd37118a2419f08d54b3f04d66304d8c91
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-04-19 21:25:25 -0300

    Exemplo de como criar um componente, utilizando o Material Design

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+Vue.component('md-card', {
+    props: ['title', 'body', 'button'],
+    data() {
+        return {
+            isVisible: true
+        }
+    },
+    methods: {
+        hideCard() {
+            this.isVisible = false;
+        }
+    },
+    template: `
+    <div class="demo-card-square mdl-card mdl-shadow--2dp" v-show="isVisible">
+        <div class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">{{ title }}</h2>
+        </div>
+        <div class="mdl-card__supporting-text">{{ body }}</div>
+        <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                {{ button }}
+            </a>
+        </div>
+        <div class="mdl-card__menu">
+            <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect" @click="hideCard">
+              <i class="material-icons">share</i>
+            </button>
+        </div>
+    </div>
+   `
+});
+
 Vue.component('task-list', {
     template: `
     <div>
